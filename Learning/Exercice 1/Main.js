87778041fc3831dec201a7a5835b532bdbcc4a89
Reissad <leopivotproduction@gmail.com>
2023-09-26 17:36:24 +0200

    add learning ex 1 et 2

---
+++ Learning/Exercice 1/Main.js
@@ -0,0 +1,16 @@
+var a = 4
+var b = 10
+var c = 30
+
+
+console.log("ca marche")
+
+if (a == b) {
+    console.log("C'est égal")
+} 
+if (c < b && a + c != 3) {
+    console.log("ok")
+} else {
+    console.log("raté")
+}
+
---
+++ Learning/Main.js
@@ -0,0 +1,18 @@
+alert("NE QUITE PAS !")
+
+console.log("bienvenue dans la console, installe toi")
+
+var name = "johnsmith"
+var money = 1000
+
+money = money + 1000
+
+if (money >= 500) {
+    console.log ("true")
+} else {
+    console.log("false")
+}
+
+
+
+console.log(money) 
