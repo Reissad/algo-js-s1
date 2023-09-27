351ea75ecdfa9fdb01d5ae76233769b392c8cb32
Reissad <leopivotproduction@gmail.com>
2023-09-27 14:04:20 +0200

    ALL EXERCCE

---
+++ Learning/Exercice 4/Main.js
@@ -0,0 +1,14 @@
+a =3
+let i =0
+while(a<9){
+    a++
+    console.log(a)
+    if (a==8){
+        break
+    }
+    if (a==7){
+        continue
+    }          
+
+    console.log(a) 
+}
---
+++ Learning/Exercie 3/Main.js
@@ -0,0 +1,7 @@
+
+a = 20
+
+for( let i = 1; i <=a; i++) {
+    console.log("okml")
+    console.log(i)
+}
